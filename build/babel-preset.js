b83ea70a564d338bdfd4cb66b9e8d2e47d5d48eb
bigfanjs <bigfanjs@gmail.com>
2020-09-19 15:58:39 +0100

    trying to fixthe build #2

--- build/babel-preset.js
+++ build/babel-preset.js
@@ -4,6 +4,19 @@ const building = BABEL_ENV != undefined && BABEL_ENV !== "cjs";
 const plugins = [
   "@babel/plugin-proposal-class-properties",
   "@babel/plugin-proposal-object-rest-spread",
+  "dev-expression",
+  [
+    "transform-react-remove-prop-types",
+    {
+      mode: "unsafe-wrap",
+    },
+  ],
+  [
+    "transform-inline-environment-variables",
+    {
+      include: ["COMPAT"],
+    },
+  ],
 ];
 
 module.exports = function () {
